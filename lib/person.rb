6645fc8c4d2a9bee42de3012477a8fd80b9ff15a
Michael <Mlcorrieri@gmail.com>
2021-05-02 17:54:05 -0400

    done.

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,9 @@
 class Person
-  #your code here
+  def initialize(attributes)
+    attributes.each do |key, value|
+      self.class.attr_accessor(key)
+      self.send(("#{key}="),value) 
+      # what does line 5 do?
+    end
+  end
 end
